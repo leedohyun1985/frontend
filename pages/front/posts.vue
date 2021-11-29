c9b5905befcd4b7739a3617708325504350d0c75
Lee Dohyun <leedohyun1985@gmail.com>
2021-11-29 22:29:59 +0900

    부트스트랩 초기 세팅

---
+++ pages/front/posts.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+    <p>body</p>
+    <p>{{ MODE }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout (context) {
+    return 'blog'
+  },
+  mounted () {
+    this.MODE = process.env.MODE
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,69 @@
 <template>
-  <div>
-    <p>{{ MODE }}</p>
-  </div>
+  <main>
+    <div>
+      <b-carousel
+        id="carousel-fade"
+        style="text-shadow: 0px 0px 2px #000"
+        fade
+        indicators
+        img-width="1024"
+        img-height="480"
+      >
+        <b-carousel-slide
+          caption="First slide"
+          img-src="https://picsum.photos/1024/480/?image=10"
+        />
+        <b-carousel-slide
+          caption="Second Slide"
+          img-src="https://picsum.photos/1024/480/?image=12"
+        />
+        <b-carousel-slide
+          caption="Third Slide"
+          img-src="https://picsum.photos/1024/480/?image=22"
+        />
+      </b-carousel>
+      <b-jumbotron>
+        <template #header>
+          BootstrapVue
+        </template>
+
+        <template #lead>
+          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+          featured content or information.
+        </template>
+
+        <hr class="my-4">
+
+        <p>
+          It uses utility classes for typography and spacing to space content out within the larger
+          container.
+        </p>
+
+        <b-button variant="primary" href="#">
+          Do Something
+        </b-button>
+        <b-button variant="success" href="#">
+          Do Something Else
+        </b-button>
+      </b-jumbotron>
+    </div>
+    <b-button v-b-toggle.collapse-1 variant="primary">
+      Toggle Collapse
+    </b-button>
+    <b-collapse id="collapse-1" class="mt-2">
+      <b-card>
+        <p class="card-text">
+          Collapse contents Here
+        </p>
+        <b-button v-b-toggle.collapse-1-inner size="sm">
+          Toggle Inner Collapse
+        </b-button>
+        <b-collapse id="collapse-1-inner" class="mt-2">
+          <b-card>Hello!</b-card>
+        </b-collapse>
+      </b-card>
+    </b-collapse>
+  </main>
 </template>
 
 <script>
@@ -17,7 +79,7 @@ export default {
   methods: {
   },
   css: [
-    '@/assets/css/main.css'
+    '@/assets/css/carousel.css'
   ]
 }
 </script>
