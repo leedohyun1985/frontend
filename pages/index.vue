7905fce47ee1841bb0a99a44029e12f6f72a04f2
Lee Dohyun <leedohyun1985@gmail.com>
2021-12-01 22:35:32 +0900

    탭 기능 구현

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,60 @@
 <template>
   <main>
     <div>
-      <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
-        <template #header>
-          BootstrapVue
-        </template>
-
-        <template #lead>
-          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
-          featured content or information.
-        </template>
-
-        <hr class="my-4">
-
-        <p>
-          It uses utility classes for typography and spacing to space content out within the larger
-          container.
-        </p>
-      </b-jumbotron>
-
+      <b-card no-body>
+        <b-tabs
+          card
+          fill
+          active-nav-item-class="font-weight-bold text-uppercase text-dark"
+          active-tab-class="font-weight-bold text-dark"
+        >
+          <b-tab
+            v-for="(theme, themeIndex) in fixMainTheme"
+            :key="themeIndex"
+            :title="theme.themeNm"
+          >
+            <b-card-group deck>
+              <b-card
+                v-for="(item, index) in theme.itemList"
+                :key="index"
+                :img-src="'https://image.homeplus.kr/it/' + item.itemNo + 's0374'"
+                img-alt="item.itemNm"
+                img-top
+                style="max-width: 20rem;"
+                class="mb-2"
+              >
+                <b-form-rating v-model="item.grade" readonly stars="5" show-value />
+                <b-badge v-if="item.recomMsg" variant="danger">
+                  {{ item.recomMsg }}
+                </b-badge>
+                <b-card-text>
+                  {{ item.itemNm }}
+                </b-card-text>
+              </b-card>
+            </b-card-group>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
+    <!-- <div>
       <b-card
-        title="Card Title"
-        img-src="https://picsum.photos/600/300/?image=25"
-        img-alt="Image"
+        v-for="(item, index) in itemList"
+        :key="index"
+        :img-src="'https://image.homeplus.kr/it/' + item.itemNo + 's0374'"
+        img-alt="item.itemNm"
         img-top
-        tag="article"
         style="max-width: 20rem;"
         class="mb-2"
       >
+        <b-form-rating v-model="item.grade" readonly stars="5" show-value />
+        <b-badge v-if="item.recomMsg" variant="danger">
+          {{ item.recomMsg }}
+        </b-badge>
         <b-card-text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
+          {{ item.itemNm }}
         </b-card-text>
-
-        <b-button href="#" variant="primary">
-          Go somewhere
-        </b-button>
       </b-card>
-    </div>
+    </div> -->
   </main>
 </template>
 
@@ -45,7 +63,8 @@ export default {
   data () {
     return {
       lionMainTheme: [],
-      fixMainTheme: []
+      fixMainTheme: [],
+      itemList: []
     }
   },
   computed: {
@@ -57,16 +76,17 @@ export default {
   methods: {
     async fetchSomething () {
       const jsonList = await this.$axios.$get('https://front.homeplus.co.kr/homeThemePeriod.json')
-      console.log(jsonList)
+      // console.log(jsonList)
       // console.log(jsonList.data[0])
       // console.log(jsonList.data[1])
       this.lionMainTheme = jsonList.data[0].lionMainTheme
       this.fixMainTheme = jsonList.data[1].fixMainTheme
-      console.log(this.lionMainTheme)
-      console.log(this.fixMainTheme)
-      for (const key in this.fixMainTheme) {
-        console.log(key)
-      }
+      console.log(this.lionMainTheme[0].itemList)
+      this.itemList = this.lionMainTheme[0].itemList
+      // console.log(this.fixMainTheme)
+      this.itemList.forEach((element) => {
+        console.log(element.itemNm)
+      })
     }
   },
   css: [
