56d6a8f05cd7c14e0a0623c7546cb943fe5fb64f
Lee Dohyun <leedohyun1985@gmail.com>
2022-01-02 21:08:47 +0900

    Change project

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,20 +1,45 @@
 <template>
-  <div class="container">
+  <v-app dark>
     <h1 v-if="error.statusCode === 404">
-      Page not found
+      {{ pageNotFound }}
     </h1>
     <h1 v-else>
-      An error occurred
+      {{ otherError }}
     </h1>
     <NuxtLink to="/">
       Home page
     </NuxtLink>
-  </div>
+  </v-app>
 </template>
 
 <script>
 export default {
-  layout: 'blog',
-  props: ['error'] // you can set a custom layout for the error page
+  name: 'EmptyLayout',
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred'
+    }
+  },
+  head () {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title
+    }
+  }
 }
 </script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,83 @@
 <template>
-  <main>
-    <div>
-      <b-card no-body>
-        <b-tabs
-          card
-          fill
-          active-nav-item-class="font-weight-bold text-uppercase text-dark"
-          active-tab-class="font-weight-bold text-dark"
-        >
-          <b-tab
-            v-for="(theme, themeIndex) in fixMainTheme"
-            :key="themeIndex"
-            :title="theme.themeNm"
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+        <VuetifyLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p>
+            For more information on Vuetify, check out the <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              documentation
+            </a>.
+          </p>
+          <p>
+            If you have questions, please join the official <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="chat"
+            >
+              discord
+            </a>.
+          </p>
+          <p>
+            Find a bug? Report it on the github <a
+              href="https://github.com/vuetifyjs/vuetify/issues"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="contribute"
+            >
+              issue board
+            </a>.
+          </p>
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <b-card-group deck>
-              <b-card
-                v-for="(item, index) in theme.itemList"
-                :key="index"
-                :img-src="'https://image.homeplus.kr/it/' + item.itemNo + 's0374'"
-                img-alt="item.itemNm"
-                img-top
-                style="max-width: 20rem;"
-                class="mb-2"
-              >
-                <b-form-rating v-model="item.grade" readonly stars="5" show-value />
-                <b-badge v-if="item.recomMsg" variant="danger">
-                  {{ item.recomMsg }}
-                </b-badge>
-                <b-card-text>
-                  {{ item.itemNm }}
-                </b-card-text>
-              </b-card>
-            </b-card-group>
-          </b-tab>
-        </b-tabs>
-      </b-card>
-    </div>
-    <!-- <div>
-      <b-card
-        v-for="(item, index) in itemList"
-        :key="index"
-        :img-src="'https://image.homeplus.kr/it/' + item.itemNo + 's0374'"
-        img-alt="item.itemNm"
-        img-top
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
-        <b-form-rating v-model="item.grade" readonly stars="5" show-value />
-        <b-badge v-if="item.recomMsg" variant="danger">
-          {{ item.recomMsg }}
-        </b-badge>
-        <b-card-text>
-          {{ item.itemNm }}
-        </b-card-text>
-      </b-card>
-    </div> -->
-  </main>
+            Nuxt Documentation
+          </a>
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      lionMainTheme: [],
-      fixMainTheme: [],
-      itemList: []
-    }
-  },
-  computed: {
-
-  },
-  mounted () {
-    this.fetchSomething()
-  },
-  methods: {
-    async fetchSomething () {
-      const jsonList = await this.$axios.$get('https://front.homeplus.co.kr/homeThemePeriod.json')
-      // console.log(jsonList)
-      // console.log(jsonList.data[0])
-      // console.log(jsonList.data[1])
-      this.lionMainTheme = jsonList.data[0].lionMainTheme
-      this.fixMainTheme = jsonList.data[1].fixMainTheme
-      console.log(this.lionMainTheme[0].itemList)
-      this.itemList = this.lionMainTheme[0].itemList
-      // console.log(this.fixMainTheme)
-      this.itemList.forEach((element) => {
-        console.log(element.itemNm)
-      })
-    }
-  },
-  css: [
-    '@/assets/css/carousel.css'
-  ]
+  name: 'IndexPage'
 }
 </script>
